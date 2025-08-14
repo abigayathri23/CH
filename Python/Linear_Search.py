fae92b9b138c3ed5dfdbee0543d5f1b66a44eb30
Abi Gayathri <abigayathri23@gmail.com>
2025-08-14 10:08:31 +0530

    Initial

---
+++ Python/Linear_Search.py
@@ -0,0 +1,14 @@
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
+
+arr = list(map(int, input("Enter the array elements: ").split()))
+target = int(input("Enter the target element: "))
+index = linear_search(arr, target)
+
+if index != -1:
+    print(f"Element found at index {index}")
+else:
+    print("Element not found in the array")
